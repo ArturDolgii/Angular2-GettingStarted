585258eef1e513a75dcf58b5d70d5adf2f879154
Artur Dolgii <adolgii@softserveinc.com>
2019-08-01 14:31:49 +0300

    * small improvements to product-list; * 06. data binding, pipes

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,6 +6,10 @@ import {Component} from "@angular/core";
 })
 export class ProductListComponent {
     pageTitle = "Product List";
+    imageWidth = 50;
+    imageMargin = 2;
+    showImage = false;
+    listFilter = "cart";
     products: any[] = [
         {
             "productId": 1,
@@ -58,4 +62,8 @@ export class ProductListComponent {
             "imageUrl": "https://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
         }
     ];
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 }
