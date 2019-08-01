28edef38d0d3e78bd6fb03c25e0438c8e45ef2eb
Artur Dolgii <adolgii@softserveinc.com>
2019-08-01 15:13:57 +0300

    * added interface, pipe, styles to product-list; * 07. more on components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import {NgModule} from "@angular/core";
 import {AppComponent} from "./app.component";
 import {ProductListComponent} from "./products/product-list.component";
 import {FormsModule} from "@angular/forms";
+import {ProductFilterPipe} from "./products/product-filter.pipe";
 
 @NgModule({
     declarations: [
         AppComponent,
-        ProductListComponent
+        ProductListComponent,
+        ProductFilterPipe
     ],
     imports: [
         BrowserModule,
