91bf9adcd4a9bb13395c8c6f17f7ea151ed7c4a3
Artur Dolgii <adolgii@softserveinc.com>
2019-08-01 16:20:39 +0300

    * added service for getting products; * 09. services and dependency injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {ProductListComponent} from "./products/product-list.component";
 import {FormsModule} from "@angular/forms";
 import {ProductFilterPipe} from "./products/product-filter.pipe";
 import {StarComponent} from "./shared/star.component";
+import {ProductService} from "./products/product.service";
 
 @NgModule({
     declarations: [
@@ -18,7 +19,8 @@ import {StarComponent} from "./shared/star.component";
         BrowserModule,
         FormsModule
     ],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
+    providers: [ProductService]
 })
 export class AppModule {
 }
